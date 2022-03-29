8bd9dc10784d1bc8f2180811ee4d82805ff2e8f4
Lucas Masiero <lucas.masi.ero@hotmail.com>
2022-03-29 09:21:18 -0300

    base application

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,18 @@
-use actix_web::{App, HttpServer, };
+// LIBS
+use actix_web::{App, HttpServer};
 use api::routes;
 
 
+// APP Server
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     HttpServer::new(|| {
+        // Router
         App::new()
             .service(routes::scope_root())
             .service(routes::scope_api())
     })
-    .bind("127.0.0.1:8080")?
+    .bind(("0.0.0.0", 8080))?
     .run()
     .await
 }
--- src/routes/mod.rs
+++ src/routes/mod.rs
@@ -4,14 +4,17 @@ pub mod root;
 pub mod api;
 
 
+// ROUTER
 pub fn scope_root() -> Scope {
     let scope = web::scope("/")
+                    // services
                     .route("", web::get().to(root::root));
     scope
 }
 
 pub fn scope_api() -> Scope {
     let scope = web::scope("/api")
+                    // services
                     .service(api::echo)
                     .service(api::hello);
     scope
